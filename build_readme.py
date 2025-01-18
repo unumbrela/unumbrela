4182a7830b7d03dc6340520719d5fe3149b9a3d2
unumbrela <zihao3351@gmail.com>
2025-01-18 19:32:13 +0800

    first commit

---
+++ build_readme.py
@@ -0,0 +1,68 @@
+import feedparser
+import httpx
+import pathlib
+import re
+
+# blog_feed_url = "https://ssscode.com/atom.xml"
+# blog_feed_url = "https://serverless-page-bucket-ybyz8f6i-1258454899.cos-website.ap-hongkong.myqcloud.com/atom.xml"
+blog_feed_url = "https://js-banana.github.io/blog/atom.xml"
+wakatime_raw_url = "https://gist.githubusercontent.com/JS-banana/b4b79e0deb0164edaae772ecbc5bd8bc/raw/"
+
+root = pathlib.Path(__file__).parent.resolve()
+
+
+def replace_chunk(content, marker, chunk, inline=False):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(
+        marker, chunk, marker)
+    return r.sub(chunk, content)
+
+
+# 已废弃：读取项目下文件的方案
+# def fetch_code_time():
+#     timeTxt = root / "packages/wakatime/time.txt"
+#     timeTxt_contents = timeTxt.open(encoding='UTF-8').read()
+#     return timeTxt_contents
+
+def fetch_code_time():
+    return httpx.get(wakatime_raw_url)
+
+# 优化展示长度
+def elipisisString(str):
+  if len(str) > 18:
+    return str[:18] + '...'
+  return str
+
+def fetch_blog_entries():
+    entries = feedparser.parse(blog_feed_url)["entries"]
+    return [
+        {
+            "title": elipisisString(entry["title"]),
+            "titleAll": entry["title"],
+            "url": entry["link"].split("#")[0],
+            "published": entry["published"].split("T")[0],
+        }
+        for entry in entries
+    ]
+
+
+if __name__ == "__main__":
+    readme = root / "README.md"
+    readme_contents = readme.open(encoding='UTF-8').read()
+
+    code_time_text = "\n```text\n"+fetch_code_time().text+"\n```\n"
+    rewritten = replace_chunk(readme_contents, "code_time", code_time_text)
+
+    entries = fetch_blog_entries()[:5]
+    entries_md = "\n".join(
+        ["* <a href='{url}' target='_blank' title='{titleAll}'>{title}</a> - {published}".format(
+            **entry) for entry in entries]
+    )
+    rewritten = replace_chunk(rewritten, "blog", entries_md)
+
+    readme.open("w", encoding='UTF-8').write(rewritten)
---
+++ packages/README.md
@@ -0,0 +1,7 @@
+# 各功能包
+
+## wakatime
+
+使用时间统计
+
+已由 <https://github.com/JS-banana/waka-box> 仓库单独处理
